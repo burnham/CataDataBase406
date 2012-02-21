fbddd10a8dca26ecb3f932c7b452c73dba52be81
misimouse <misimouse.dol@gmail.com>
2012-02-21 12:02:36 +0100

    SAI: Golgarok

--- ArkDB_Updates/ArkDB_uptades_r1.2.sql
+++ ArkDB_Updates/ArkDB_uptades_r1.2.sql
@@ -85,3 +85,15 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,3500,4600,11,96201,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player are in range cast - Web Wrap"),
 (@ENTRY,@SOURCETYPE,1,0,0,0,100,0,2500,3500,9500,10000,11,86570,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Shale Flurry"),
 (@ENTRY,@SOURCETYPE,2,0,0,0,100,0,5500,5600,11000,12500,11,83381,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: kill");
+
+-- SAI: Golgarok
+SET @ENTRY := 50059;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,10,3000,3500,11,86699,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player on range cast: Sockwave"),
+(@ENTRY,@SOURCETYPE,1,0,2,0,100,0,25,25,35000,48000,11,80117,0,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"On 25% HP: Earth Spike"),
+(@ENTRY,@SOURCETYPE,2,0,67,0,100,0,10000,11000,0,0,11,80182,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On behing target cast: Uppercut"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,25000,28000,60000,70000,11,86861,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Earthquake");

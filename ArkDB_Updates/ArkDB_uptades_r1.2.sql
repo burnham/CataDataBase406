413b16d3461ab2b30eaf4eee7898c7b90d114616
misimouse <misimouse.dol@gmail.com>
2012-02-21 18:41:26 +0100

    SAI: Burgy Blackheart

--- ArkDB_Updates/ArkDB_uptades_r1.2.sql
+++ ArkDB_Updates/ArkDB_uptades_r1.2.sql
@@ -130,3 +130,14 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,0,0,4,0,100,0,0,0,0,0,11,53148,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On aggro: Charge"),
 (@ENTRY,@SOURCETYPE,1,0,0,0,100,0,1300,1500,10000,15000,11,67982,4,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Claw"),
 (@ENTRY,@SOURCETYPE,2,0,2,0,100,0,50,50,0,0,11,93082,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On 50% HP cast: Supernerf");
+
+-- SAI: Burgy Blackheart
+SET @ENTRY := 50052;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,1,30,1200,1300,11,75380,0,0,0,0,0,0,0,0,0,0.0,0.0,0.0,0.0,"On Range cast: Parrrley!"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,1600,1700,12000,15000,11,75361,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC Cast: Swashbuckling Slice"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,1200,1300,28000,30000,11,79414,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC Cast: Brittle Touch");

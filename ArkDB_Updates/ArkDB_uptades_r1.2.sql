9a3bc7599eb17d9fc07f3845f10be5568cd8e118
misimouse <misimouse.dol@gmail.com>
2012-02-21 18:11:43 +0100

    SAI: Ghostcrawler

--- ArkDB_Updates/ArkDB_uptades_r1.2.sql
+++ ArkDB_Updates/ArkDB_uptades_r1.2.sql
@@ -119,3 +119,14 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,1,0,0,0,100,0,5500,6000,120000,130000,11,93491,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Algae"),
 (@ENTRY,@SOURCETYPE,2,0,2,0,100,0,40,40,24000,25400,11,8599,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On 40% HP cast: Enrage"),
 (@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2500,3000,60000,120000,11,93494,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Wake");
+
+-- SAI: Ghostcrawler
+SET @ENTRY := 50051;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,4,0,100,0,0,0,0,0,11,53148,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On aggro: Charge"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,1300,1500,10000,15000,11,67982,4,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Claw"),
+(@ENTRY,@SOURCETYPE,2,0,2,0,100,0,50,50,0,0,11,93082,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On 50% HP cast: Supernerf");

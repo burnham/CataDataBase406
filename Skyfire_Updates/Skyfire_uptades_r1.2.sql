8ad9e13270883acabe504f69feb60e6bf8264312
misimouse <misimouse.dol@gmail.com>
2012-02-21 11:01:11 +0100

    SAI: Sambas

--- Skyfire_Updates/Skyfire_uptades_r1.2.sql
+++ Skyfire_Updates/Skyfire_uptades_r1.2.sql
@@ -64,4 +64,13 @@ SET @SOURCETYPE := 0;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,0,0,11,75002,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player range - Cast Leaping Rush");
+(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,1000,2500,11,75002,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player range - Cast Leaping Rush");
+
+-- SAI: Sambas
+SET @ENTRY := 50159 ;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,1000,2500,11,75002,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player range - Cast Leaping Rush");

e7f34e5487889d2a9168954a5faa7b7a3b473983
misimouse <misimouse.dol@gmail.com>
2012-02-21 11:23:58 +0100

    SAI; Jadefang

--- Skyfire_Updates/Skyfire_uptades_r1.2.sql
+++ Skyfire_Updates/Skyfire_uptades_r1.2.sql
@@ -74,3 +74,14 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCE
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,1000,2500,11,75002,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player range - Cast Leaping Rush");
+
+-- SAI; Jadefang
+SET @ENTRY := 49822;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,45,3500,4600,11,96201,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On player are in range cast - Web Wrap"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,2500,3500,9500,10000,11,86570,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Shale Flurry"),
+(@ENTRY,@SOURCETYPE,2,0,0,0,100,0,5500,5600,11000,12500,11,83381,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: kill");

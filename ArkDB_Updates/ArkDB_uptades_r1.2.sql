d65a0d7fafa9d7aa29f7337cfe886c2b7913dd31
misimouse <misimouse.dol@gmail.com>
2012-02-21 14:28:17 +0100

    SAI: Mobus

--- ArkDB_Updates/ArkDB_uptades_r1.2.sql
+++ ArkDB_Updates/ArkDB_uptades_r1.2.sql
@@ -107,3 +107,15 @@ UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,0,0,100,0,1000,60000,65000,70000,11,79927,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Earth Shield"),
 (@ENTRY,@SOURCETYPE,1,0,0,0,100,0,5000,6000,45000,50000,11,32738,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Bore");
+
+-- SAI: Mobus
+SET @ENTRY := 50009;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,1300,2300,60000,70000,11,93492,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Ram"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,5500,6000,120000,130000,11,93491,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Algae"),
+(@ENTRY,@SOURCETYPE,2,0,2,0,100,0,40,40,24000,25400,11,8599,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On 40% HP cast: Enrage"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,2500,3000,60000,120000,11,93494,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Wake");

b12ee833472fd0ac7742c0d3f973e8e6d6f2fba3
misimouse <misimouse.dol@gmail.com>
2012-02-21 12:43:59 +0100

    SAI: Terborus

--- Skyfire_Updates/Skyfire_uptades_r1.2.sql
+++ Skyfire_Updates/Skyfire_uptades_r1.2.sql
@@ -97,3 +97,13 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,1,0,2,0,100,0,25,25,35000,48000,11,80117,0,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"On 25% HP: Earth Spike"),
 (@ENTRY,@SOURCETYPE,2,0,67,0,100,0,10000,11000,0,0,11,80182,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On behing target cast: Uppercut"),
 (@ENTRY,@SOURCETYPE,3,0,0,0,100,0,25000,28000,60000,70000,11,86861,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Earthquake");
+
+-- SAI: Terborus
+SET @ENTRY := 50060;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,1000,60000,65000,70000,11,79927,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Earth Shield"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,5000,6000,45000,50000,11,32738,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC cast: Bore");

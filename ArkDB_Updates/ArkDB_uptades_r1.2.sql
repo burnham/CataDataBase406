3defe38b3cc3907a6f3655a63ca323cf3a6e94a4
misimouse <misimouse.dol@gmail.com>
2012-02-21 19:22:34 +0100

    SAI: Xariona

--- ArkDB_Updates/ArkDB_uptades_r1.2.sql
+++ ArkDB_Updates/ArkDB_uptades_r1.2.sql
@@ -172,3 +172,15 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,0,0,9,0,100,0,5,30,1200,1300,11,0,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On Range Cast: Spiked Charge"),
 (@ENTRY,@SOURCETYPE,1,0,0,0,100,0,1500,1600,40000,45000,11,93592,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On Ic Cast: Diligeddon"),
 (@ENTRY,@SOURCETYPE,2,0,0,0,100,0,1600,1700,16000,20000,11,93590,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On Ic Cast: Flame Breath");
+
+-- SAI: Xariona
+SET @ENTRY := 50061;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,1500,1600,25000,30000,11,93544,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On IC Cast: Twilight Breath"),
+(@ENTRY,@SOURCETYPE,1,0,0,0,100,0,3500,4000,55000,60000,11,93546,0,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"On IC Cast: Twilight Fissure"),
+(@ENTRY,@SOURCETYPE,2,0,2,0,100,0,50,50,25000,30000,11,93553,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On 50% HP Cast: Twilight Zone"),
+(@ENTRY,@SOURCETYPE,3,0,0,0,100,0,4000,4500,60000,70000,11,93556,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Ic Cast: Unleashed Magic");

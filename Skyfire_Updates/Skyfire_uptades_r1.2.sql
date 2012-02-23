dda7c5e575300e9bcc76310c8bc3c496deac8528
misimouse <misimouse.dol@gmail.com>
2012-02-23 12:17:59 +0100

    TYPO: Miss delete one  guids for reload the SQL

--- Skyfire_Updates/Skyfire_uptades_r1.2.sql
+++ Skyfire_Updates/Skyfire_uptades_r1.2.sql
@@ -7,7 +7,7 @@
 -- NPC
 -- -----
 -- Lieutenant Walden miss correct phases on 1 and 2
-DELETE FROM `creature` WHERE `guid` IN (4833,6716,6565507,6721,6929,7279,7247,7283);
+DELETE FROM `creature` WHERE `guid` IN (4833,6716,6565507,6721,6929,7279,7247,7282,7283);
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
 (4833, 34863, 638, 1, 1, 0, 0, -1405.52, 1445.8, 35.5562, 2.99158, 600, 0, 0, 42, 0, 0, 0, 0, 0),
 (6716, 34863, 638, 1, 2, 0, 0, -1405.52, 1445.8, 35.5562, 2.99158, 600, 0, 0, 42, 0, 0, 0, 0, 0),
